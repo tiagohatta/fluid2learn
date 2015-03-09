6fb89c48da3e82cf9866c48b8601ca7400fd3aa4
Tiago Hatta <tiago.eidi@gmail.com>
2015-03-09 12:51:26 -0300

    Ultimas modificações

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -26,11 +26,9 @@ public class Enquirer implements IEnquirer
 		// perguntas ja respondidas sao tratadas por hashing
 		Map<String, String> jaRespondidas = new HashMap<String, String>();
 		
+		// vetor que guarda todos os animais que estao no jogo
         String[] listaAnimais = bc.listaNomes(); 
-        
-        
         int i = 0;
-		
         boolean animalEsperado = false;
 		
         while(!animalEsperado){
@@ -38,13 +36,17 @@ public class Enquirer implements IEnquirer
         	IDeclaracao decl = obj.primeira();
 
         	animalEsperado = true;
+        
         	while (decl != null && animalEsperado) {
-            
+    
         		String pergunta = decl.getPropriedade();
+        		
+        		// se a pergunta a ser feita nao foi respondida anteriormente, ela eh feita 
         		if(!(jaRespondidas.containsKey(pergunta))){
-        	
+
         			String respostaEsperada = decl.getValor();
         			String resposta = responder.ask(pergunta);
+        			
         			if (resposta.equalsIgnoreCase(respostaEsperada))
         				decl = obj.proxima();
         			else
@@ -52,9 +54,11 @@ public class Enquirer implements IEnquirer
         	
         			jaRespondidas.put(pergunta, resposta);
         		}
+        		// ignora a pergunta, caso seja repetida, passando para a proxima disponivel 
         		else
         			decl = obj.proxima();
         	}
+        
         	i++;
         }
         	
